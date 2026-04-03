32aa54be2ae152bddf6e1ed1ac043f818a2d896d
meltingseaweed <conor.seo@gmail.com>
2026-04-04 08:28:25 +0900

    Build: Add basic movement to white_pawn and testing to the movement
    Add forward movement to white_pawn one or two
    spaces unless blocked by any piece.
    Cannot Capture diagonally yet or en passant.

--- spec/white_pawn_spec.rb
+++ spec/white_pawn_spec.rb
@@ -1,19 +1,31 @@
 # frozen_string_literal: true
 require 'pry-byebug'
 require_relative '../lib/all_pieces'
+require_relative '../lib/board'
 require_relative '../lib/pieces/white_pawn'
 
 RSpec.describe WhitePawn do
   describe 'Evaluates possible moves' do
-    subject(:white_pawn) { WhitePawn.new }
+    subject(:pawn_two) { WhitePawn.new("w", [6,2]) }
+    subject(:pawn_three) { WhitePawn.new("w", [5,3]) }
+    subject(:board) { Board.new }
     context 'will calculate only legal moves under basic circumstances' do
-      xit 'can move 1 or 2 spaces if in original start position' do
+      it 'can move 1 or 2 spaces if in original start position' do
+        moves = pawn_two.movement_pawn(board.chessboard)
+        expect(moves).to eql([[5,2], [4,2]])
       end
 
-      xit 'can move 1 space forward unless blocked' do
+      it 'can move 1 space forward when not in start position' do
+        moves = pawn_three.movement_pawn(board.chessboard)
+        expect(moves).to eql([[4,3]])
       end
-      xit 'cannot move forward when space in front is blocked' do 
+
+      it 'cannot move forward when space in front is blocked' do
+        board.chessboard[5][2] = WhitePawn.new("w", [5,2])
+        moves = pawn_two.movement_pawn(board.chessboard)
+        expect(moves).to eql([]) 
       end
+
       xit 'can move diagonally 1 space to take an enemy piece' do
       end
     end
