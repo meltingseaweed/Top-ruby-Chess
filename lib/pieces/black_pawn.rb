9e02ff0e0528d731afd660710b03c1878545cba2
meltingseaweed <conor.seo@gmail.com>
2026-04-04 08:35:19 +0900

    Build: Remake black_pawn basic movement downwards and include testing
    The same as white_pawn except movement is down
    not up. The same testing was done.
    Cannot capture diagonally yet or en passant

--- lib/pieces/black_pawn.rb
+++ lib/pieces/black_pawn.rb
@@ -1,34 +1,33 @@
-module Chess
-  class BlackPawn < Pieces
+class BlackPawn < Pieces
 
-    def initialize(position)
-      @position = position
-      @team = "b"
-    end
+  def initialize(team, position)
+    @position = position
+    @team = team
+  end
 
-    def movement_pawn
-      # 3 basic scenarios.
-      # Consider the square in front empty if it is equal to nil
-      # If it has an object inside, it will not be equal to nil
-      # 1: Can move two or one spaces if in starting position
-      # 2: One move forward
-      # 3: Diagonal move to capture
-      possible_moves = []
-      row = @position[0]
-      col = @position[1]
-      # Code for movement scenario 1
-      if row == 1
-        possible_moves << [row + 1, col] if [row + 1, col].nil?
-        possible_moves << [row + 2, col] if [row + 2, col].nil?
-      elsif row > 1 # Code for movement scenario 2
-        possible_moves << [row + 1, col] if [row + 1, col].nil?
-      end
-      # Code for scenario 3
-      if [row + 1, col - 1] && col >= 0 # && team is white
-        possible_moves << [row + 1, col - 1]
-      elsif [row + 1, col + 1] && col <= 7 # && team is white
-        possible_moves << [row + 1, col + 1]
-      end
+  def movement_pawn(chessboard)
+    # 3 basic scenarios.
+    # Consider the square in front empty if it is equal to nil
+    # If a piece is there it will not be equal to nil
+    # 1: Can move two or one spaces if in starting position
+    # 2: One move forward
+    # 3: Diagonal move to capture
+    possible_moves = []
+    row = @position[0]
+    col = @position[1]
+    # Code for movement scenario 1
+    if row == 1
+      possible_moves << [row + 1, col] if chessboard[row + 1][col].nil?
+      possible_moves << [row + 2, col] if chessboard[row + 2][col].nil? && chessboard[row + 1][col].nil?
+    elsif row > 1 # Code for movement scenario 2
+      possible_moves << [row + 1, col] if chessboard[row + 1][col].nil?
     end
+    # Code for scenario 3
+    # if chessboard[row - 1][col - 1] && col > 0 # && team is black
+    #   capture_array << [row - 1, col - 1]
+    # elsif chessboard[row - 1][col + 1] && col < 7 # && team is black
+    #   capture_array << [row - 1, col + 1]
+    # end
+    possible_moves
   end
 end
---
+++ spec/black_pawn_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+require 'pry-byebug'
+require_relative '../lib/all_pieces'
+require_relative '../lib/board'
+require_relative '../lib/pieces/black_pawn'
+
+RSpec.describe BlackPawn do
+  describe 'Evaluates possible moves' do
+    subject(:pawn_two) { BlackPawn.new("b", [1,2]) }
+    subject(:pawn_three) { BlackPawn.new("b", [2,3]) }
+    subject(:board) { Board.new }
+    context 'will calculate only legal moves under basic circumstances' do
+      it 'can move 1 or 2 spaces if in original start position' do
+        moves = pawn_two.movement_pawn(board.chessboard)
+        expect(moves).to eql([[2,2], [3,2]])
+      end
+
+      it 'can move 1 space forward when not in start position' do
+        moves = pawn_three.movement_pawn(board.chessboard)
+        expect(moves).to eql([[3,3]])
+      end
+
+      it 'cannot move forward when space in front is blocked' do
+        board.chessboard[2][2] = BlackPawn.new("b", [3,2])
+        moves = pawn_two.movement_pawn(board.chessboard)
+        expect(moves).to eql([]) 
+      end
+
+      xit 'can move diagonally 1 space to take an enemy piece' do
+      end
+    end
+
+    context 'when pawn reaches the other side' do
+      xit 'may be switched for another piece' do
+        
+      end
+    end
+
+  end
+end
