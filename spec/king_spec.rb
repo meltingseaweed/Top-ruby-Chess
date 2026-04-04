be2d9a37b74832660bdc9d9534f46c39d9a2016a
meltingseaweed <conor.seo@gmail.com>
2026-04-04 09:21:45 +0900

    Testing: Add testing for King movement
    Add tests for basic one space movement of the
    king. Does not test for capturing yet.
    Does not test for castling yet.

---
+++ spec/king_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+require 'pry-byebug'
+require_relative '../lib/board'
+require_relative '../lib/all_pieces'
+require_relative '../lib/pieces/king'
+
+RSpec.describe King do
+  describe 'Evaluates possible moves' do
+    subject(:king) { King.new("w", [7,4]) }
+    subject(:king_two) { King.new("w", [6,4]) }
+    subject(:board) { Board.new }
+    context 'will return legal moves' do
+      it 'Can return no moves on the first turn' do
+        board.chessboard[7][3] = King.new("w", [7,3])
+        board.chessboard[7][5] = King.new("w", [7,5])
+        board.chessboard[6][3] = King.new("w", [6,3])
+        board.chessboard[6][4] = King.new("w", [6,4])
+        board.chessboard[6][5] = King.new("w", [6,5])
+        moves = king.movement_king(board.chessboard)
+        expect(moves).to eql([])
+      end
+      it 'Can return moves in all directions if no pieces surround it' do
+        moves = king_two.movement_king(board.chessboard)
+        expect(moves).to eql([[5,4], [5,5], [6,5], [7,5], [7,4], [7,3], [6,3], [5,3]])
+      end
+
+      xit 'Can return array of capturable pieces' do
+        
+      end
+
+      xit 'Can capture a piece' do
+        
+      end
+    end
+
+    context 'correctly finds check' do
+      xit 'Correctly finds check to be true' do
+        
+      end
+
+      xit 'Correctly finds check to be false' do
+        
+      end
+    end
+  end
+end
