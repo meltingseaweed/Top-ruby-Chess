c8a569f7d51fa0bc50af755f81c689a206b9a50e
meltingseaweed <conor.seo@gmail.com>
2026-04-06 07:07:35 +0900

    Build: Add capture method knight
    Add method to find enemy pieces that can be
    captured by the knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -3,6 +3,8 @@ require_relative '../board'
 
 class Knight
   
+  attr_reader :team
+  
   def initialize(team, position)
     @team = team
     @position = position
@@ -22,5 +24,22 @@ class Knight
       end
     end
     new_positions
+  end
+
+  def capturable(chessboard)
+    enemy_pieces = []
+    row = @position[0]
+    col = @position[1]
+    movement_array = [[1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,-1], [-2,1], [-1,2]]
+    movement_array.each do |x, y|
+      new_row = row + x
+      new_col = col + y
+      if new_row.between?(0,7) && new_col.between?(0,7)
+        if chessboard[new_row][new_col] != nil
+          enemy_pieces << [new_row, new_col] if chessboard[new_row][new_col].team != @team
+        end
+      end
+    end
+    enemy_pieces
   end
 end
