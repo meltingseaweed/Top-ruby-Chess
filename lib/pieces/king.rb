a6379eea2bf4b7302a99e0fad8bf2c536640e9da
meltingseaweed <conor.seo@gmail.com>
2026-04-07 07:15:23 +0900

    Build: Add method to find enemy pieces
    Add method to find enemy pieces from current
    position.

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -3,6 +3,7 @@ require_relative '../all_pieces'
 
 class King
   
+  attr_reader :team
   def initialize(team, position)
     @team = team
     @position = position
@@ -18,9 +19,25 @@ class King
       new_col = col + y
       if new_row.between?(0,7) && new_col.between?(0,7)
         new_positions << [new_row, new_col] if chessboard[new_row][new_col].nil?
-        # Add to capture array if that position contains an enemy piece
       end
     end
     new_positions
+  end
+
+  def capturable(chessboard)
+    enemy_pieces = []
+    row = @position[0]
+    col = @position[1]
+    movement_array = movement_array = [[-1,0], [-1,1], [0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1]]
+    movement_array.each do |x, y|
+      new_row = row + x
+      new_col = col + y
+      if new_row.between?(0,7) && new_col.between?(0,7)
+        if chessboard[new_row][new_col] != nil
+          enemy_pieces << [new_row, new_col] if chessboard[new_row][new_col].team != @team
+        end
+      end
+    end
+    enemy_pieces
   end
 end
