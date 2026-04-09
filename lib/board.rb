95383895f11f7c47149b35ccf7d225b85a5fe1c3
meltingseaweed <conor.seo@gmail.com>
2026-04-10 07:12:06 +0900

    Build: Add display_board method
    Add method to clone the chessboard and covert the
    objects into chess representations to be displayed
    before every move

--- lib/board.rb
+++ lib/board.rb
@@ -38,85 +38,86 @@ class Board
       @chessboard[0][4] = King.new("b", [0,4])
       @chessboard[7][4] = King.new("w", [7,4])
       count = 0
-      8.times do 
+      while count < 9 do 
         @chessboard[1][count] = BlackPawn.new("b", [1, count])
         @chessboard[6][count] = WhitePawn.new("w", [6, count])
         count += 1
       end
       count = 0
       letter = 97
-      8.times do 
+      while count < 9 do 
         @chessboard[count][8] = "| #{count} |"
         @chessboard[8][count] = "| #{(letter+count).chr} |"
         count += 1
       end
-      @chessboard[8][8] = "|   |"
     end
     
-    def create_display_practice
-      @chessboard.each do |row|
-        row.each do |val|
-          if val.nil 
+    def display_board
+      display = @chessboard.map(&:clone)
+      display = display.map do |row|
+        row.map do |val|
+          if val.nil?
             val = "|   |"
-            next
-          end
-          # Use 2 if statements to control the team selection
-          # For each case, change the value of val to the corrent unicode
-          
-          # How do we access the chessboard / Do we make another display board?
-          if val.team == "b"
-            case val.piece
-            when "black_pawn" then val = "| \u265F |"
-            when "rook" then val = "| \u265C |"
-            when "knight" then val = "| \u265E |"
-            when "bishop" then val = "| \u265D |"
-            when "queen" then val = "| \u265B |"
-            when "king" then val = "| \u265A |"
+          else
+            if val.is_a?(String)
+              val = val
+            elsif val.team == "b"
+              case val.piece
+              when "black_pawn" then val = "| \u265F |"
+              when "rook" then val = "| \u265C |"
+              when "knight" then val = "| \u265E |"
+              when "bishop" then val = "| \u265D |"
+              when "queen" then val = "| \u265B |"
+              when "king" then val = "| \u265A |"
+              end
+            elsif val.team == "w"
+              case val.piece
+              when "white_pawn" then val = "| \u2659 |"
+              when "rook" then val = "| \u2656 |"
+              when "knight" then val = "| \u2658 |"
+              when "bishop" then val = "| \u2657 |"
+              when "queen" then val = "| \u2655 |"
+              when "king" then val = "| \u2654 |"
+              end
             end
           end
-
-          if val.team == "w"
-            case val
-          end
-          case val
-          when val.nil then val = "|   |"
-          when val.piece
-          end
+      
         end
       end
-
-
-
-
-      @chessboard[0][0] = @chessboard[0][7] = "| \u265C |"
-      @chessboard[0][1] = @chessboard[0][6] = "| \u265E |"
-      @chessboard[0][2] = @chessboard[0][5] = "| \u265D |"
-      @chessboard[0][3] = "| \u265B |"
-      @chessboard[0][4] = "| \u265A |"
-      @chessboard[1] = @chessboard[1].map { |piece| piece = "| \u265F |" }
-      @chessboard[7][0] = @chessboard[7][7] = "| \u2656 |"
-      @chessboard[7][1] = @chessboard[7][6] = "| \u2658 |"
-      @chessboard[7][2] = @chessboard[7][5] = "| \u2657 |"
-      @chessboard[7][3] = "| \u2655 |"
-      @chessboard[7][4] = "| \u2654 |"
-      @chessboard[6] = @chessboard[6].map { |piece| piece = "| \u2659 |" }
-      count = 8
-      @chessboard.each do |row|
-        row[8] = "| #{count} |"
-        count -= 1
+      binding.pry
+      display.each do |row|
+        print "#{row}\n"
       end
     end
+end
+    #   @chessboard[0][0] = @chessboard[0][7] = "| \u265C |"
+    #   @chessboard[0][1] = @chessboard[0][6] = "| \u265E |"
+    #   @chessboard[0][2] = @chessboard[0][5] = "| \u265D |"
+    #   @chessboard[0][3] = "| \u265B |"
+    #   @chessboard[0][4] = "| \u265A |"
+    #   @chessboard[1] = @chessboard[1].map { |piece| piece = "| \u265F |" }
+    #   @chessboard[7][0] = @chessboard[7][7] = "| \u2656 |"
+    #   @chessboard[7][1] = @chessboard[7][6] = "| \u2658 |"
+    #   @chessboard[7][2] = @chessboard[7][5] = "| \u2657 |"
+    #   @chessboard[7][3] = "| \u2655 |"
+    #   @chessboard[7][4] = "| \u2654 |"
+    #   @chessboard[6] = @chessboard[6].map { |piece| piece = "| \u2659 |" }
+    #   count = 8
+    #   @chessboard.each do |row|
+    #     row[8] = "| #{count} |"
+    #     count -= 1
+    #   end
+    # end
   
 
-    def display_board
-      @chessboard.each do |row|
-        row.each do |value|
-          print value
-        end
-        puts ""
-      end
-      puts "----------------------------------------"
-      puts "| a || b || c || d || e || f || g || h |"
-    end
-  end
+    # def display_board
+    #   @chessboard.each do |row|
+    #     row.each do |value|
+    #       print value
+    #     end
+    #     puts ""
+    #   end
+    #   puts "----------------------------------------"
+    #   puts "| a || b || c || d || e || f || g || h |"
+    # end
 # end
