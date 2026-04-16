3cbf4f60a2eadd4d945d5cc14a5f233cbee965fc
meltingseaweed <conor.seo@gmail.com>
2026-04-16 10:41:48 +0900

    Debug: Fix error in displaying readable positions
    Change number array so the row number is displayed
    correctly

--- lib/chess_methods.rb
+++ lib/chess_methods.rb
@@ -21,11 +21,11 @@ end
 
 def convert_to_readable(array)
   letter_array = %w(a b c d e f g h)
-  num_array = [7, 6, 5, 4, 3, 2, 1, 0]
+  num_array = [8, 7, 6, 5, 4, 3, 2, 1]
   if array.include?("castleleft")
     array.delete("castleleft")
     new = array.map do |x, y|
-    x = num_array[x-1].to_s
+    x = num_array[x].to_s
     y = letter_array[y]
     ["#{y}#{x}"]
   end
@@ -33,18 +33,18 @@ def convert_to_readable(array)
   elsif array.include?("castleright")
     array.delete("castleright")
     new = array.map do |x, y|
-    x = num_array[x-1].to_s
+    x = num_array[x].to_s
     y = letter_array[y]
     ["#{y}#{x}"]
   end
   array.push("castleright")
   else
   new = array.map do |x, y|
-    x = num_array[x-1].to_s
+    x = num_array[x].to_s
     y = letter_array[y]
     ["#{y}#{x}"]
   end
   end
-  
+
 end
 end
